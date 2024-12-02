b7ffd63593062e11f20fe7db54e86d6eccbc9490
Arngrame <arngrame.dev@gmail.com>
2024-12-03 00:24:23 +0400

    Value annotation

--- src/main/java/ru/job4j/MoodBotApplication.java
+++ src/main/java/ru/job4j/MoodBotApplication.java
@@ -5,21 +5,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import ru.job4j.api.telegram.TelegramBotService;
-import ru.job4j.content.Content;
+import ru.job4j.config.AppConfig;
 
 @SpringBootApplication
-public class Main {
+public class MoodBotApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(MoodBotApplication.class, args);
     }
 
     @Bean
-    public CommandLineRunner initTelegramApi(ApplicationContext ctx) {
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-            var bot = ctx.getBean(TelegramBotService.class);
-            bot.receive(new Content());
+            AppConfig appConfig = ctx.getBean(AppConfig.class);
+            appConfig.printConfig();
         };
     }
 }
