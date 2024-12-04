49cc8a0e7bc37feb4ce6579dff79eca26904bfff
Arngrame <arngrame.dev@gmail.com>
2024-12-04 23:24:06 +0400

    Spring data added

--- src/main/java/ru/job4j/api/model/User.java
+++ src/main/java/ru/job4j/api/model/User.java
@@ -1,9 +1,26 @@
 package ru.job4j.api.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "mb_user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "client_id")
     private long clientId;
+
+    @Column(name = "chat_id")
     private long chatId;
 
     public User() {
@@ -37,5 +54,24 @@ public class User {
 
     public void setChatId(long chatId) {
         this.chatId = chatId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
--- src/main/java/ru/job4j/api/storage/UserRepository.java
+++ src/main/java/ru/job4j/api/storage/UserRepository.java
@@ -1,14 +1,17 @@
 package ru.job4j.api.storage;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import ru.job4j.api.model.User;
 
 import java.util.List;
 
-public interface UserRepository {
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
 
     List<User> findAll();
 
     User findByClientId(Long clientId);
 
-    void save(User user);
+    User save(User user);
 }
