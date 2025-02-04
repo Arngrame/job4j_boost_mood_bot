2864b8b2ae67cb9a44253ce35708c6e3860b98f3
Arngrame <arngrame.dev@gmail.com>
2025-02-04 18:22:47 +0400

    BotCommandHandler updated

--- src/main/java/ru/job4j/api/telegram/BotCommandHandler.java
+++ src/main/java/ru/job4j/api/telegram/BotCommandHandler.java
@@ -1,24 +1,61 @@
 package ru.job4j.api.telegram;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import ru.job4j.api.content.Content;
+import ru.job4j.api.model.User;
+import ru.job4j.api.services.MoodService;
+import ru.job4j.api.storage.UserRepository;
+
+import java.util.Optional;
 
 @Service
 public class BotCommandHandler {
 
-    void receive(Content content) {
-        System.out.println(content);
+    private final UserRepository userRepository;
+    private final MoodService moodService;
+    private final TelegramUI telegramUI;
+
+    public BotCommandHandler(UserRepository userRepository,
+                             MoodService moodService,
+                             TelegramUI telegramUI) {
+        this.userRepository = userRepository;
+        this.moodService = moodService;
+        this.telegramUI = telegramUI;
     }
 
-    @PostConstruct
-    public void init() {
-        System.out.println("BotCommandHandler bean is going through init.");
+    Optional<Content> commands(Message message) {
+        var chatId = message.getChatId();
+        var clientId = message.getFrom().getId();
+        var command = message.getText();
+
+        return switch (command) {
+            case "/start" -> handleStartCommand(chatId, clientId);
+            case "/week_mood_log" -> moodService.weekMoodLogCommand(chatId, clientId);
+            case "/month_mood_log" -> moodService.monthMoodLogCommand(chatId, clientId);
+            case "/award" -> moodService.awards(chatId, clientId);
+            default -> Optional.empty();
+        };
     }
 
-    @PreDestroy
-    public void destroy() {
-        System.out.println("BotCommandHandler bean will be destroyed now.");
+    Optional<Content> handleCallback(CallbackQuery callback) {
+        Long moodId = Long.valueOf(callback.getData());
+        User user = userRepository.findByClientId(callback.getFrom().getId());
+        Content content = moodService.chooseMood(user, moodId);
+        return Optional.of(content);
+    }
+
+    private Optional<Content> handleStartCommand(long chatId, Long clientId) {
+        User user = new User();
+        user.setClientId(clientId);
+        user.setChatId(chatId);
+        userRepository.save(user);
+
+        var content = new Content(user.getChatId());
+        content.setText("Как настроение?");
+        content.setMarkup(telegramUI.buildButtons());
+
+        return Optional.of(content);
     }
 }
