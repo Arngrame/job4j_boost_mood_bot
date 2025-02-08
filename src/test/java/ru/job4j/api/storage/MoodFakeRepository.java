aca004a21433600a06c9d2c9d002578782155601
Arngrame <arngrame.dev@gmail.com>
2025-02-09 00:04:23 +0400

    @Autowired & @Qualifier

--- src/test/java/ru/job4j/api/storage/MoodFakeRepository.java
+++ src/test/java/ru/job4j/api/storage/MoodFakeRepository.java
@@ -1,11 +1,13 @@
 package ru.job4j.api.storage;
 
+import org.springframework.stereotype.Component;
 import org.springframework.test.fake.CrudRepositoryFake;
 import ru.job4j.api.model.Mood;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class MoodFakeRepository extends CrudRepositoryFake<Mood, Long> implements MoodRepository {
 
     public List<Mood> findAll() {
---
+++ src/test/java/ru/job4j/api/telegram/TelegramUITest.java
@@ -0,0 +1,37 @@
+package ru.job4j.api.telegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import ru.job4j.api.storage.MoodFakeRepository;
+import ru.job4j.api.storage.MoodRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+@ContextConfiguration(classes = {TelegramUI.class, MoodFakeRepository.class})
+class TelegramUITest {
+
+    @Autowired
+    private TelegramUI telegramUI;
+
+    @MockBean
+    private MoodRepository moodRepository;
+
+    @Test
+    public void whenBtnGood() {
+        assertThat(moodRepository).isNotNull();
+    }
+
+    @Test
+    public void whenButtonBuildSuccessfully() {
+        telegramUI.buildButtons();
+        verify(moodRepository, times(1)).findAll();
+    }
+
+}
